da26ebee87e29d9c67e087c054d5dd81c8bf3493
broop <soundwanders@gmail.com>
2020-10-19 15:22:00 -0400

    created second input for expense item name, both append to expenseContainer on submit. need to put in alerts and stops for NaN and non-text strings in Expense Name input (expense item name not allowed to be a number)

--- budgetscript.js
+++ budgetscript.js
@@ -5,23 +5,33 @@ const budgetWrapper = document.querySelector('.wrapper');
 
 //--Create Divs for Budget Balance, Expenses, and Income--//
 //create a div that will hold new expenses
-const expenseContainer = document.createElement('div')
-//create an input to add new expense
+const expenseContainer = document.createElement('div');
+//create input to add new expense name
+const expenseName = document.createElement('INPUT');
+//create an input to add new expense $$ amount
 const inputExpense = document.createElement('INPUT');
-//set class of div and input
-    // setting class allows me to use it in CSS and HTML and manipulate
-expenseContainer.classList.add('expenseContainer');
+
+// set expenseName class , id , type
+expenseName.classList.add('expenseName');
+expenseName.id = 'expName';
+expenseName.setAttribute('type', 'text');
+//set inputExpense class , id , type
 inputExpense.classList.add('inputExpense');
-inputExpense.id = 'newExp'
-//add text node. this WILL be BLANK, text is placeholder for now
+inputExpense.id = 'newExp';
+inputExpense.setAttribute('type', 'text');
+//set class for expenseContainer
+expenseContainer.classList.add('expenseContainer');
+//add text node to expenseContainer. this will need to hold expense items...array??
 expenseContainer.textContent = 'Add New Expense';
-// append divs to parent div budgetWrapper
+
+// append div and inputs to parent budgetWrapper
 budgetWrapper.appendChild(expenseContainer);
+budgetWrapper.appendChild(expenseName);
 budgetWrapper.appendChild(inputExpense);
 
 // Total Expenses
 //create a div that will show total expenses
-const allExpensesContainer = document.createElement('div')
+const allExpensesContainer = document.createElement('div');
 allExpensesContainer.classList.add('allExpensesContainer');
 allExpensesContainer.textContent = 'Total Expenses';
 budgetWrapper.appendChild(allExpensesContainer);
@@ -31,10 +41,11 @@ budgetWrapper.appendChild(allExpensesContainer);
 const incomeContainer = document.createElement('div')
 const setIncome = document.createElement('INPUT');
 incomeContainer.classList.add('incomeContainer');
-incomeContainer.textContent = 'Income here';
+incomeContainer.textContent = 'Income';
 setIncome.classList.add('setIncome');
-setIncome.id = 'newInc'
+setIncome.id = 'newInc';
 setIncome.setAttribute('type', 'text');
+
 budgetWrapper.appendChild(incomeContainer);
 budgetWrapper.appendChild(setIncome);
 
@@ -45,30 +56,41 @@ const budgetContainer = document.createElement('div');
 budgetContainer.classList.add('budgetContainer');
 budgetContainer.setAttribute('type' , 'text');
 budgetContainer.textContent = 'Budget Balance';
-budgetWrapper.appendChild(budgetContainer);
 
+budgetWrapper.appendChild(budgetContainer);
 
 //-- Create Div to show Expense items then append to budgetWrapper --//
     // this will need to be refined to actually pull in all the expense items
 const expenseList = document.createElement('div');
 expenseList.classList.add('expenseList');
 expenseList.textContent = 'Show All Expenses';
+
 budgetWrapper.appendChild(expenseList);
 
 ////////////////////////////////////// 
 
+// SUBMIT INCOME Function
 function submitIncome() {
-    newIncome = document.getElementById("newInc").value;
+    newIncome = document.getElementById('newInc').value;
     incomeContainer.innerHTML = '';
     incomeContainer.textContent = '';
 
     incomeContainer.append(newIncome);
 };
 
+// SUBMIT EXPENSE AMOUNT and NAME Function
 function submitExpense() {
-    newExpense = document.getElementById("newExp").value;
+    newExpense = document.getElementById('newExp').value;
+    expItemName = document.getElementById('expName').value;
+
     expenseContainer.innerHTML = '';
     expenseContainer.textContent = '';
 
-    expenseContainer.append(newExpense);
+    expenseContainer.append(expItemName);
+    expenseContainer.append(' ' + parseInt(newExpense));
+    
+    if (newExpense == 0) {
+        alert("Expense amount must be a number greater than zero");
+        return;
+    }
 };
