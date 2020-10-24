3d6d0fc31919b2960c6b6051a976080f78d10244
broop <soundwanders@gmail.com>
2020-10-24 15:15:32 -0400

    submit function checks fall working, continue testing to try to break . //  need to create a place to hold all expenses and design a UI to make it look nice and easily accessible

--- budgetscript.js
+++ budgetscript.js
@@ -12,11 +12,11 @@ const expenseName = document.createElement('INPUT');
 const inputExpense = document.createElement('INPUT');
 
 // set expenseName class , id , type
-expenseName.classList.add('expenseName');
+expenseName.classList.add('inputs');
 expenseName.id = 'expName';
 expenseName.setAttribute('type', 'text');
 //set inputExpense class , id , type
-inputExpense.classList.add('inputExpense');
+inputExpense.classList.add('inputs');
 inputExpense.id = 'newExp';
 inputExpense.setAttribute('type', 'text');
 //set class for expenseContainer
@@ -42,7 +42,7 @@ const incomeContainer = document.createElement('div')
 const setIncome = document.createElement('INPUT');
 incomeContainer.classList.add('incomeContainer');
 incomeContainer.textContent = 'Income';
-setIncome.classList.add('setIncome');
+setIncome.classList.add('inputs');
 setIncome.id = 'newInc';
 setIncome.setAttribute('type', 'text');
 
@@ -60,6 +60,10 @@ budgetContainer.textContent = 'Budget Balance';
 budgetWrapper.appendChild(budgetContainer);
 
 
+// reg exp is used in submit functions to check if input is a number between 0-9
+var regEx = /^[0-9]+$/;
+
+
 //-- Create Div to show Expense items then append to budgetWrapper --//
     // this will need to be refined to actually pull in all the expense items
 const expenseList = document.createElement('div');
@@ -73,10 +77,28 @@ budgetWrapper.appendChild(expenseList);
 // SUBMIT INCOME Function
 function submitIncome() {
     newIncome = document.getElementById('newInc').value;
-    incomeContainer.innerHTML = '';
-    incomeContainer.textContent = '';
 
-    incomeContainer.append(newIncome);
+    if (newIncome == 0 || '') {
+        alert("Income amount cannot be zero or empty");
+        
+    } else if (!newIncome.match(regEx)) {
+        alert("Income amount must be a number");
+// if income's first value is 0 and next value is > 0,
+// then slice 0 and append rest as floating point number
+    } else if (newIncome[0] == 0 && newIncome[1].match(regEx)) {
+        incomeContainer.innerHTML = '';
+        incomeContainer.textContent = ''; 
+
+        incomeContainer.append(expItemName);
+        newIncome.slice[0];
+        incomeContainer.append(parseFloat(newIncome));
+
+    } else {
+    incomeContainer.innerHTML = '';
+    incomeContainer.textContent = '';  
+
+    incomeContainer.append(parseFloat(newIncome));
+    }
 };
 
 // SUBMIT EXPENSE AMOUNT and NAME Function
@@ -84,14 +106,26 @@ function submitExpense() {
     newExpense = document.getElementById('newExp').value;
     expItemName = document.getElementById('expName').value;
 
-    if (newExpense != 0) {
+ if (newExpense == 0 || '') {
+        alert("Expense amount cannot be zero or empty");
+
+    } else if (!newExpense.match(regEx)) {
+        alert("Expense amount must be a number");
+// if expense number's first value is 0 and next value is > 0,
+// slice 0, then parseFloat and append the rest
+    } else if (newExpense[0] == 0 && newExpense[1].match(regEx)) {
+        expenseContainer.innerHTML = '';
+        expenseContainer.textContent = ''; 
+
+        expenseContainer.append(expItemName);
+        newExpense.slice[0];
+        expenseContainer.append(' ' + parseFloat(newExpense));
+
+    } else {
     expenseContainer.innerHTML = '';
-    expenseContainer.textContent = '';
+    expenseContainer.textContent = '';  
 
     expenseContainer.append(expItemName);
-    expenseContainer.append(' ' + parseInt(newExpense));
-
-    } else if (newExpense == 0) {
-        alert("Expense amount must be a number greater than zero");
+    expenseContainer.append(' ' + parseFloat(newExpense));
     }
 };
